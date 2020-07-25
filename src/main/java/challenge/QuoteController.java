e59a1e6c2b61823fa1611fd31f77c14dad943b25
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-24 23:35:29 -0300

    corrige url da api

--- src/main/java/challenge/QuoteController.java
+++ src/main/java/challenge/QuoteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/quote")
+@RequestMapping(value = "/v1/quote")
 public class QuoteController {
 
 	@Autowired
